7448daa7f948194b19315294c6f4a3e959708cbd
vlad0308 <makushenkovladislav199@gmail.com>
2022-07-26 12:36:19 +0300

    Добавьте файлы проекта.

---
+++ Myclass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Myclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Makushenko avatar = new Makushenko();
+            avatar.Can();
+
+        }
+    }
+    class Makushenko
+    {
+        string name;
+        string sourname;
+        int old;
+        float weight;
+
+        public void Can()
+        {
+            Console.WriteLine($"name : {name}");
+            Console.WriteLine($"sourname : {sourname}");
+            Console.WriteLine($"")
+        }
+    }
+}
